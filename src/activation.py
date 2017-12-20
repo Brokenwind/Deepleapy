4a671f543ba6df0edd4b698faacf10de75daf1ed
Brokenwind <wangkun6536@163.com>
2017-12-20 20:29:09 +0800

    Add some common activation function

---
+++ src/activation.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+import scipy.io
+import sklearn
+import sklearn.datasets
+
+def sigmoid(x):
+    """
+    Compute the sigmoid of x
+
+    Arguments:
+    x -- A scalar or numpy array of any size.
+
+    Return:
+    s -- sigmoid(x)
+    """
+    s = 1.0/(1.0+np.exp(-x))
+    return s
+
+def relu(x):
+    """
+    Compute the relu of x
+
+    Arguments:
+    x -- A scalar or numpy array of any size.
+
+    Return:
+    s -- relu(x)
+    """
+    s = np.maximum(0,x)
+
+    return s
