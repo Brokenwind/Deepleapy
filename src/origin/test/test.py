9bf241b015fc1ffe36a975ad84cf657d2375656e
Brokenwind <wangkun6536@163.com>
2017-12-23 22:33:24 +0800

    Used the deep network to compute a model to recognize the handwritten digits

--- src/origin/test/test.py
+++ src/origin/test/test.py
@@ -1,4 +1,5 @@
 import sys
+import time
 sys.path.append('../')
 sys.path.append('../../')
 from neural import *
@@ -14,9 +15,34 @@ def load_params(fpath):
 
     return params
 
+def fitparams(X,Y):
+    hyperparams = {}
+    units = [400, 25, 10]
+    hyperparams['units'] = units
+    hyperparams['activition'] = 'relu'
+    hyperparams['classifier'] = 'sigmoid'
+    hyperparams['lamda'] = 1.0
+    hyperparams['iters'] = 500
+    hyperparams['alpha'] = 0.2
+
+    tic = time.process_time()
+
+    params = gradient_descent(hyperparams,X,Y)
+
+    toc = time.process_time()
+    print ("----- Computation time = " + str(1000*(toc - tic)) + "ms")
+
+    # test forward propagation
+    Y1 = predict(params,hyperparams,X,Y)
+    y1 = map.matrix2index(Y1)
+    y0 = map.matrix2index(Y)
+    accuracy = np.sum(y1 == y0)/5000.0
+    print ('The accuracy on hand-written digit dataset is: {}%'.format( accuracy * 100 ))
+
+
 if __name__ == '__main__':
     fpath = '../../../dataset/hand_written_digit.npz'
-    map = DataMap([1,2,3,4,5,6,7,8,9,10])
+    map = DataMap([1,2,3,4,5,6,7,8,9,0])
     data = np.load(fpath)
     params = load_params(fpath)
 
@@ -29,16 +55,18 @@ if __name__ == '__main__':
     units = [400, 25, 10]
     hyperparams['units'] = units
     hyperparams['activition'] = 'sigmoid'
-    hyperparams['lamda'] = 0.1
-
+    hyperparams['classifier'] = 'sigmoid'
+    hyperparams['lamda'] = 0.0
+    # test cost function
     cost = compute_cost(params,hyperparams,X,Y)
     print('The cost value on test data is: ',cost)
-
+    # test forward propagation
     Y1 = predict(params,hyperparams,X,Y)
     y1 = map.matrix2index(Y1)
     y0 = map.matrix2index(Y)
-
     accuracy = np.sum(y1 == y0)/5000.0
     print ('The accuracy on hand-written digit dataset is: {}%'.format( accuracy * 100 ))
-
+    # check backward propagation
     check_gradient()
+
+    fitparams(X,Y)
