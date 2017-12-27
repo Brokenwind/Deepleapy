14ee6c0546d98eb3a2cb9beb7c90e730e452d2f2
Brokenwind <wangkun6536@163.com>
2017-12-27 15:17:37 +0800

    Maked program more rubust

--- src/datamap.py
+++ src/datamap.py
@@ -2,8 +2,8 @@ from pandas import Series,DataFrame
 import numpy as np
 
 class DataMap:
-    # the list of original classes
-    classes = []
+    # a array of original classes
+    classes = None
     map = None
     # the index of classes
     index = None
@@ -12,8 +12,8 @@ class DataMap:
     offset = 0
 
     def __init__(self,classes, offset = 0):
-        self.classes = classes
-        self.class_num = len(classes)
+        self.classes = np.array(classes).flatten()
+        self.class_num = self.classes.size
         self.offset = offset
         self.index = np.arange(0,self.class_num) + self.offset
         self.map = Series(self.index,index=classes)
@@ -26,9 +26,18 @@ class DataMap:
 
     def class2index(self, data):
         """
-        index the data
+        Tranform a class array to its index array
+        Parameters
+        ----------
+        data : list/tuple/np.ndarray
+            a array/tuple/array of class names
+        Returns
+        -------
+        res : np.ndarray
+            a array of transformed indexes
         """
-        num = len(data)
+        data = np.array(data)
+        num = data.size
         res = np.zeros(num, dtype=int)
         for i in range(0,num):
             res[i] = self.map[data[i]]
@@ -37,8 +46,17 @@ class DataMap:
 
     def index2class(self,idx):
         """
-        covert index to its corresponding class
+        Transform index to its corresponding class
+        Parameters
+        idx : list/tuple/np.ndarray
+            a array/tuple/array of indexes
+        ----------
+        Returns
+        -------
+        res : np.ndarray
+            a list of transformed class names
         """
+        idx = np.array(idx).flatten()
         num = len(idx)
         idx = np.array(idx) - self.offset
         res = num * [None]
@@ -50,8 +68,16 @@ class DataMap:
 
     def class2matrix(self,data):
         """
-        convert a list of classes to matrix expression
+        Transform a list of class names to matrix expression
         It is used when class number bigger than 2
+        Parameters
+        ----------
+        data : list/tuple/np.ndarray
+            a array/tuple/array of class names
+        Returns
+        -------
+        res : 2D np.ndarray(classes,samples)
+            a 2D array of transformed matrix
         """
         idx = self.class2index(data)
         res = self.index2matrix(idx)
@@ -59,34 +85,36 @@ class DataMap:
 
     def matrix2class(self,matrix):
         """
-        convert a matrix expression to a list of classes
-        It is used when class number bigger than 2
+        Transform a matrix expression to a list of class names
+        Parameters
+        ----------
+        matrix : 2D np.ndarray(classes,samples)
+            a 2D array of transformed matrix
+        Returns
+        -------
+            a list of class names
         """
         idx = self.matrix2index(matrix)
         return self.index2class(idx)
 
-    def matrix2index(self,matrix):
-        """
-        convert a matrix expression to a list of indexes
-        It is used when class number bigger than 2
-        """
-        if matrix.shape[0] == 1 :
-            idx = self.binary_classify(matrix)
-        else:
-            idx = np.argmax(matrix,axis=0)
-            idx += self.offset
-            idx = idx.astype(int)
-
-        return idx
-
     def index2matrix(self,idx):
         """
-        convert a list of indexes to its matrix expression
-        It is used when class number bigger than 2
+        Transform a list/tuple/array of indexes to its matrix expression
+        Parameters
+        ----------
+        matrix : list/tuple/np.ndarray
+            a array/tuple/array of indexes
+        Returns
+        -------
+        res : 2D np.ndarray (classes,samples)
+            a 2D array of transformed matrix
         """
         if self.class_num == 2:
+            idx = np.array(idx)
+            idx = idx.reshape((1,idx.size))
+            if len(np.unique(idx)) != 2:
+                raise ValueError('The number of different idx is not consistent with the number of different class name')
             return idx
-
         num = np.size(idx)
         idx = idx - self.offset
         idx = idx.astype(int)
@@ -97,12 +125,36 @@ class DataMap:
 
         return res
 
-    def binary_classify(self,data):
-        data[data >= 0.5] = 1
-        data[data < 0.5] = 0
-        data = data.astype(int)
+    def matrix2index(self,matrix):
+        """
+        Transform matrix expression of label to a indexes array
+        Parameters
+        ----------
+        matrix : 2D np.ndarray (classes,samples)
+            a 2D array of transformed matrix
+        Returns
+        -------
+        idx : np.ndarray
+            a array of indexes
+        """
+        matrix = np.array(matrix)
+        if matrix.ndim == 1 or matrix.shape[0] == 1 or matrix.shape[0] == 1:
+            matrix[matrix >= 0.5] = 1
+            matrix[matrix <  0.5] = 0
+            idx = matrix.astype(int)
+        else:
+            idx = np.argmax(matrix,axis=0)
+            idx += self.offset
+            idx = idx.astype(int)
 
-        return data
+        return idx
+
+    def matrix2matrix(self,matrix):
+        """
+        Transform a probility matrix to a matrix with 0 and 1
+        """
+        idx = self.matrix2index(matrix)
+        return self.index2matrix(idx)
 
 if __name__ == '__main__':
     map = DataMap(['one','two','three','four'],offset=1)
@@ -111,3 +163,7 @@ if __name__ == '__main__':
     print (map.index2class([1,2,3,4]))
     print (map.class2matrix(['one','two','three','four']))
     print (map.matrix2class(np.eye(4)))
+
+    map = DataMap(['zero','one'])
+    print (map.index2matrix([0,1,0,1]))
+    print (map.matrix2index(np.array([[0,1,0,1]])))
