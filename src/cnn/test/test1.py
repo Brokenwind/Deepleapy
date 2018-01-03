0598335fc12db85548ae1a586e243f65c4a351ce
Brokenwind <wangkun6536@163.com>
2018-01-03 16:24:49 +0800

    Added and tested the function to do pooling

--- src/cnn/test/test1.py
+++ src/cnn/test/test1.py
@@ -21,11 +21,39 @@ def test_conv():
     print(np.mean(A_prev))
     print(np.mean(W))
     print(np.mean(b))
-    hparameters = {"pad" : 2, "stride": 2}
-    Z = conv(A_prev, W, b, pad=2, stride=2)
+    hyperparams = {"conv_pad" : 2, "conv_stride": 2}
+    Z = conv(A_prev, W, b, hyperparams)
     print(Z.shape)
     print("Z's mean = ", np.mean(Z))
     print("Z[3,2,1] =", Z[3,2,1])
 
+def test_pool():
+    """
+    expected results:
+    max mode:
+    [[[[ 1.74481176  1.6924546   2.10025514]]]
+
+    [[[ 1.19891788  1.51981682  2.18557541]]]]
+
+    average mode:
+    [[[[-0.09498456  0.11180064 -0.14263511]]]
+
+    [[[-0.09525108  0.28325018  0.33035185]]]]
+    """
+    np.random.seed(1)
+    A_prev = np.random.randn(2, 4, 4, 3)
+    hyperparams = {"pool_stride" : 1, "pool_filter_size": 4}
+
+    hyperparams['pool_mode'] = 'max'
+    A = pool_forward(A_prev, hyperparams)
+    print("max mode:")
+    print(A)
+
+    hyperparams['pool_mode'] = 'average'
+    A = pool_forward(A_prev, hyperparams)
+    print("average mode: ")
+    print(A)
+
 if __name__ == '__main__':
-    test_conv()
+    #test_conv()
+    test_pool()
--- src/cnn/utils.py
+++ src/cnn/utils.py
@@ -24,7 +24,7 @@ def zero_pad(X,pad):
 
     return X
 
-def conv(X,W,b,pad=0,stride=1):
+def conv(X,W,b,hyperparams):
     """
     Implements the forward propagation for a convolution function
     Parameters
@@ -41,6 +41,8 @@ def conv(X,W,b,pad=0,stride=1):
     Z : conv output, numpy array of shape (m, n_H, n_W, n_C)
     cache -- cache of values needed for the conv_backward() function
     """
+    pad=hyperparams['conv_pad']
+    stride=hyperparams['conv_stride']
     filter_size = W.shape[0]
     m = X.shape[0]
     outh = 1 + int( ( X.shape[1] - filter_size + 2*pad ) / stride )
@@ -60,5 +62,67 @@ def conv(X,W,b,pad=0,stride=1):
                     startw = w * stride
                     endw = startw + filter_size
                     res[i,h,w,l] = np.sum( sample[starth:endh,startw:endw] * W[:,:,:,l] + b[:,:,:,l])
+
+    return res
+
+def pool_forward(A_prev, hyperparams):
+    """
+    Implements the forward pass of the pooling layer
+
+    Parameters
+    ----------
+    A_prev : numpy array of shape (m, n_H_prev, n_W_prev, n_C_prev)
+        Input data, m samples
+    hyperparams : python dictionary
+        must containing the following parameters:
+        pool_filter_size : int, the size of pool filter
+        pool_stride : int, the stride of pool
+        mode : string ("max" or "average"), the pooling mode you would like to use
+
+    Returns:
+    ----------
+    A : a numpy array of shape (m, n_H, n_W, n_C)
+        output of the pool layer 
+    cache : cache used in the backward pass of the pooling layer, contains the input and hyperparams
+    """
+
+    # Retrieve dimensions from the input shape
+    (m, inh, inw, inc) = A_prev.shape
+
+    # Retrieve hyperparameters from "hyperparams"
+    filter_size = hyperparams["pool_filter_size"]
+    stride = hyperparams["pool_stride"]
+    mode = hyperparams["pool_mode"]
+
+    # Define the dimensions of the output
+    outh = int(1 + (inh - filter_size) / stride)
+    outw = int(1 + (inw - filter_size) / stride)
+    outc = inc
+
+    # Initialize output matrix A
+    res = np.zeros((m, outh, outw, outc))
+    # loop over the training examples
+    for i in range(m):
+        # the current sample to be processed
+        sample = A_prev[i]
+        # loop on the vertical axis of the output volume
+        for h in range(outh):
+            # loop on the horizontal axis of the output volume
+            for w in range(outw):
+                # loop over the channels of the output volume
+                for c in range (outc):
+                    # Find the corners of the current "slice" (≈4 lines)
+                    starth = h * stride
+                    endh = starth + filter_size
+                    startw = w * stride
+                    endw = startw + filter_size
+                    # Compute the pooling operation on the slice. Use an if statment to differentiate the modes. Use np.max/np.mean.
+                    if mode == "max":
+                        res[i, h, w, c] = np.max(sample[starth:endh, startw:endw, c])
+                    elif mode == "average":
+                        res[i, h, w, c] = np.mean(sample[starth:endh, startw:endw, c])
+
+    # Store the input and hyperparams in "cache" for pool_backward()
+    #cache = (A_prev, hyperparams)
 
     return res
